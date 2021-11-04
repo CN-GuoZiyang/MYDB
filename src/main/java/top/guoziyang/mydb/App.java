36522a4db57d787eb73737795f9f6e31e94f0d58
Ziyang <guoziyang0033@gmail.com>
2021-11-05 00:04:08 +0800

    feat: 初始化maven项目

---
+++ src/main/java/top/guoziyang/mydb/App.java
@@ -0,0 +1,13 @@
+package top.guoziyang.mydb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
