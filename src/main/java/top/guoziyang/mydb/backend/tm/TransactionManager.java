a607505b86d492546685754bc8b3c670c2233f62
Ziyang <guoziyang0033@gmail.com>
2021-11-07 23:44:06 +0800

    fix: 将TransactionManager改成接口

--- src/main/java/top/guoziyang/mydb/backend/tm/TransactionManager.java
+++ src/main/java/top/guoziyang/mydb/backend/tm/TransactionManager.java
@@ -6,36 +6,20 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import top.guoziyang.mydb.backend.utils.Panic;
-import top.guoziyang.mydb.backend.utils.Parser;
 
-public class TransactionManager {
+public interface TransactionManager {
+    long begin();
+    void commit(long xid);
+    void abort(long xid);
+    boolean isActive(long xid);
+    boolean isCommitted(long xid);
+    boolean isAborted(long xid);
+    void close();
 
-    // XID文件头长度
-    private static final int LEN_XID_HEADER_LENGTH = 8;
-    // 每个事务的占用长度
-    private static final int XID_FIELD_SIZE = 1;
-
-    // 事务的三种状态
-    private static final byte FIELD_TRAN_ACTIVE   = 0;
-	private static final byte FIELD_TRAN_COMMITTED = 1;
-	private static final byte FIELD_TRAN_ABORTED  = 2;
-
-    // 超级事务，永远为commited状态
-    public static final long SUPER_XID = 0;
-
-    public static final String XID_SUFFIX = ".xid";
-    
-    private RandomAccessFile file;
-    private FileChannel fc;
-    private long xidCounter;
-    private Lock counterLock;
-
-    public static TransactionManager create(String path) {
-        File f = new File(path+XID_SUFFIX);
+    public static TransactionManagerImpl create(String path) {
+        File f = new File(path+TransactionManagerImpl.XID_SUFFIX);
         try {
             if(!f.createNewFile()) {
                 Panic.panic(new RuntimeException("File already exists!"));
@@ -57,7 +41,7 @@ public class TransactionManager {
         }
 
         // 写空XID文件头
-        ByteBuffer buf = ByteBuffer.wrap(new byte[LEN_XID_HEADER_LENGTH]);
+        ByteBuffer buf = ByteBuffer.wrap(new byte[TransactionManagerImpl.LEN_XID_HEADER_LENGTH]);
         try {
             fc.position(0);
             fc.write(buf);
@@ -65,11 +49,11 @@ public class TransactionManager {
             Panic.panic(e);
         }
         
-        return new TransactionManager(raf, fc);
+        return new TransactionManagerImpl(raf, fc);
     }
 
-    public static TransactionManager open(String path) {
-        File f = new File(path+XID_SUFFIX);
+    public static TransactionManagerImpl open(String path) {
+        File f = new File(path+TransactionManagerImpl.XID_SUFFIX);
         if(!f.exists()) {
             Panic.panic(new RuntimeException("File does not exists!"));
         }
@@ -86,144 +70,6 @@ public class TransactionManager {
            Panic.panic(e);
         }
 
-        return new TransactionManager(raf, fc);
+        return new TransactionManagerImpl(raf, fc);
     }
-
-    private TransactionManager(RandomAccessFile raf, FileChannel fc) {
-        this.file = raf;
-        this.fc = fc;
-        counterLock = new ReentrantLock();
-        checkXIDCounter();
-    }
-
-    /**
-     * 检查XID文件是否合法
-     * 读取XID_FILE_HEADER中的xidcounter，根据它计算文件的理论长度，对比实际长度
-     */
-    private void checkXIDCounter() {
-        long fileLen = 0;
-        try {
-            fileLen = file.length();
-        } catch (IOException e1) {
-            Panic.panic(new RuntimeException("Bad XID file"));
-        }
-        if(fileLen < LEN_XID_HEADER_LENGTH) {
-            Panic.panic(new RuntimeException("Bad XID file"));
-        }
-
-        ByteBuffer buf = ByteBuffer.allocate(LEN_XID_HEADER_LENGTH);
-        try {
-            fc.position(0);
-            fc.read(buf);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-        this.xidCounter = Parser.parseLong(buf.array());
-        long end = getXidPosition(this.xidCounter + 1);
-        if(end != fileLen) {
-            Panic.panic(new RuntimeException("Bad XID file"));
-        }
-    }
-
-    // 根据事务xid取得其在xid文件中对应的位置
-    private long getXidPosition(long xid) {
-        return LEN_XID_HEADER_LENGTH + (xid-1)*XID_FIELD_SIZE;
-    }
-
-    // 更新xid事务的状态为status
-    private void updateXID(long xid, byte status) {
-        long offset = getXidPosition(xid);
-        byte[] tmp = new byte[XID_FIELD_SIZE];
-        tmp[0] = status;
-        ByteBuffer buf = ByteBuffer.wrap(tmp);
-        try {
-            fc.position(offset);
-            fc.write(buf);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-        try {
-            fc.force(true);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-    }
-
-    // 将XID加一，并更新XID Header
-    private void incrXIDCounter() {
-        xidCounter ++;
-        ByteBuffer buf = ByteBuffer.wrap(Parser.long2Byte(xidCounter));
-        try {
-            fc.position(0);
-            fc.write(buf);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-        try {
-            fc.force(true);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-    }
-
-    // 开始一个事务，并返回XID
-    public long begin() {
-        counterLock.lock();
-        try {
-            long xid = xidCounter + 1;
-            updateXID(xid, FIELD_TRAN_ACTIVE);
-            incrXIDCounter();
-            return xid;
-        } finally {
-            counterLock.unlock();
-        }
-    }
-
-    // 提交XID事务
-    public void commit(long xid) {
-        updateXID(xid, FIELD_TRAN_COMMITTED);
-    }
-
-    // 回滚XID事务
-    public void abort(long xid) {
-        updateXID(xid, FIELD_TRAN_ABORTED);
-    }
-
-    // 检测XID事务是否处于status状态
-    private boolean checkXID(long xid, byte status) {
-        long offset = getXidPosition(xid);
-        ByteBuffer buf = ByteBuffer.wrap(new byte[XID_FIELD_SIZE]);
-        try {
-            fc.position(offset);
-            fc.read(buf);
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-        return buf.array()[0] == status;
-    }
-
-    public boolean isActive(long xid) {
-        if(xid == SUPER_XID) return false;
-        return checkXID(xid, FIELD_TRAN_ACTIVE);
-    }
-
-    public boolean isCommitted(long xid) {
-        if(xid == SUPER_XID) return true;
-        return checkXID(xid, FIELD_TRAN_COMMITTED);
-    }
-
-    public boolean isAborted(long xid) {
-        if(xid == SUPER_XID) return false;
-        return checkXID(xid, FIELD_TRAN_ABORTED);
-    }
-
-    public void close() {
-        try {
-            fc.close();
-            file.close();
-        } catch (IOException e) {
-            Panic.panic(e);
-        }
-    }
-
 }
