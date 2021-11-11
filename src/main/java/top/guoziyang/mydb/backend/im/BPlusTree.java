e198a08f0cc89f7afa1bdf21a9a82d91e7f55843
Ziyang <guoziyang0033@gmail.com>
2021-11-12 00:29:43 +0800

    feat: 完成B+树（未测试）

---
+++ src/main/java/top/guoziyang/mydb/backend/im/BPlusTree.java
@@ -0,0 +1,240 @@
+package top.guoziyang.mydb.backend.im;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import top.guoziyang.mydb.backend.common.SubArray;
+import top.guoziyang.mydb.backend.dm.DataManager;
+import top.guoziyang.mydb.backend.dm.dataItem.DataItem;
+import top.guoziyang.mydb.backend.im.Node.InsertAndSplitRes;
+import top.guoziyang.mydb.backend.im.Node.LeafSearchRangeRes;
+import top.guoziyang.mydb.backend.im.Node.SearchNextRes;
+import top.guoziyang.mydb.backend.tm.TransactionManagerImpl;
+import top.guoziyang.mydb.backend.utils.Parser;
+
+public class BPlusTree {
+    DataManager dm;
+    long bootUid;
+    DataItem bootDataItem;
+    Lock bootLock;
+
+    public static long create(DataManager dm) throws Exception {
+        byte[] rawRoot = Node.newNilRootRaw();
+        long rootUid = 0;
+        try {
+            rootUid = dm.insert(TransactionManagerImpl.SUPER_XID, rawRoot);
+        } catch(Exception e) {
+            throw e;
+        }
+        long bootUid = 0;
+        try {
+            bootUid = dm.insert(TransactionManagerImpl.SUPER_XID, Parser.long2Byte(rootUid));
+        } catch(Exception e) {
+            throw e;
+        }
+        return bootUid;
+    }
+
+    public static BPlusTree load(long bootUid, DataManager dm) throws Exception {
+        DataItem bootDataItem = null;
+        try {
+            bootDataItem = dm.read(bootUid);
+        } catch(Exception e) {
+            throw e;
+        }
+        assert bootDataItem != null;
+        BPlusTree t = new BPlusTree();
+        t.bootUid = bootUid;
+        t.dm = dm;
+        t.bootDataItem = bootDataItem;
+        t.bootLock = new ReentrantLock();
+        return t;
+    }
+
+    private long rootUid() {
+        bootLock.lock();
+        try {
+            SubArray sa = bootDataItem.data();
+            return Parser.parseLong(Arrays.copyOfRange(sa.raw, sa.start, sa.start+8));
+        } finally {
+            bootLock.unlock();
+        }
+    }
+
+    private void updateRootUid(long left, long right, long rightKey) throws Exception {
+        bootLock.lock();
+        try {
+
+            byte[] rootRaw = Node.newRootRaw(left, right, rightKey);
+            long newRootUid = 0;
+            try {
+                newRootUid = dm.insert(TransactionManagerImpl.SUPER_XID, rootRaw);
+            } catch(Exception e) {
+                throw e;
+            }
+            bootDataItem.before();
+            System.arraycopy(Parser.long2Byte(newRootUid), 0, bootDataItem.data().raw, bootDataItem.data().start, 8);
+            bootDataItem.after(TransactionManagerImpl.SUPER_XID);
+        } finally {
+            bootLock.unlock();
+        }
+    }
+
+    private long searchLeaf(long nodeUid, long key) throws Exception {
+        Node node = null;
+        try {
+            node = Node.loadNode(this, nodeUid);
+        } catch(Exception e) {
+            throw e;
+        }
+        boolean isLeaf = node.isLeaf();
+        node.release();
+
+        if(isLeaf) {
+            return nodeUid;
+        } else {
+            long next = 0;
+            try {
+                next = searchNext(nodeUid, key);
+            } catch(Exception e) {
+                throw e;
+            }
+            return searchLeaf(next, key);
+        }
+    }
+
+    private long searchNext(long nodeUid, long key) throws Exception {
+        while(true) {
+            Node node = null;
+            try {
+                node = Node.loadNode(this, nodeUid);
+            } catch(Exception e) {
+                throw e;
+            }
+            SearchNextRes res = node.searchNext(key);
+            node.release();
+            if(res.uid != 0) return res.uid;
+            nodeUid = res.siblingUid;
+        }
+    }
+
+    public List<Long> search(long key) throws Exception {
+        return searchRange(key, key);
+    }
+
+    private List<Long> searchRange(long leftKey, long rightKey) throws Exception {
+        long rootUid = rootUid();
+        long leafUid = 0;
+        try {
+            leafUid = searchLeaf(rootUid, leftKey);
+        } catch(Exception e) {
+            throw e;
+        }
+        List<Long> uids = new ArrayList<>();
+        while(true) {
+            Node leaf = null;
+            try {
+                leaf = Node.loadNode(this, leafUid);
+            } catch(Exception e) {
+                throw e;
+            }
+            LeafSearchRangeRes res = leaf.leafSearchRange(leftKey, rightKey);
+            leaf.release();
+            uids.addAll(res.uids);
+            if(res.siblingUid == 0) {
+                break;
+            } else {
+                leafUid = res.siblingUid;
+            }
+        }
+        return uids;
+    }
+
+    public void insert(long key, long uid) throws Exception {
+        long rootUid = rootUid();
+        InsertRes res = null;
+        try {
+            res = insert(rootUid, uid, key);
+        } catch(Exception e) {
+            throw e;
+        }
+        if(res.newNode != 0) {
+            try {
+                updateRootUid(rootUid, res.newNode, res.newKey);
+            } catch(Exception e) {
+                throw e;
+            }
+        }
+    }
+
+    class InsertRes {
+        long newNode, newKey;
+    }
+
+    private InsertRes insert(long nodeUid, long uid, long key) throws Exception {
+        Node node = null;
+        try {
+            node = Node.loadNode(this, nodeUid);
+        } catch(Exception e) {
+            throw e;
+        }
+        boolean isLeaf = node.isLeaf();
+        node.release();
+
+        InsertRes res = null;
+        if(isLeaf) {
+            res = insertAndSplit(nodeUid, uid, key);
+        } else {
+            long next = 0;
+            try {
+                next = searchNext(nodeUid, key);
+            } catch(Exception e) {
+                throw e;
+            }
+
+            InsertRes ir = null;
+            try {
+                ir = insert(next, uid, key);
+            } catch(Exception e) {
+                throw e;
+            }
+            if(ir.newNode != 0) {
+                res = insertAndSplit(nodeUid, ir.newNode, ir.newKey);
+            }
+        }
+        return res;
+    }
+
+    private InsertRes insertAndSplit(long nodeUid, long uid, long key) throws Exception {
+        while(true) {
+            Node node = null;
+            try {
+                node = Node.loadNode(this, nodeUid);
+            } catch(Exception e) {
+                throw e;
+            }
+            InsertAndSplitRes iasr = null;
+            try { 
+                iasr = node.insertAndSplit(uid, key);
+            } catch(Exception e) {
+                throw e;
+            }
+            node.release();
+            if(iasr.siblingUid != 0) {
+                nodeUid = iasr.siblingUid;
+            } else {
+                InsertRes res = new InsertRes();
+                res.newNode = iasr.newSon;
+                res.newKey = iasr.newKey;
+                return res;
+            }
+        }
+    }
+
+    public void close() {
+        bootDataItem.release();
+    }
+}
---
+++ src/main/java/top/guoziyang/mydb/backend/im/Node.java
@@ -0,0 +1,307 @@
+package top.guoziyang.mydb.backend.im;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import top.guoziyang.mydb.backend.common.SubArray;
+import top.guoziyang.mydb.backend.dm.dataItem.DataItem;
+import top.guoziyang.mydb.backend.tm.TransactionManagerImpl;
+import top.guoziyang.mydb.backend.utils.Parser;
+
+/**
+ * Node结构如下：
+ * [LeafFlag][KeyNumber][SiblingUid]
+ * [Son0][Key0][Son1][Key1]...[SonN][KeyN]
+ */
+public class Node {
+    static final int IS_LEAF_OFFSET = 0;
+    static final int NO_KEYS_OFFSET = IS_LEAF_OFFSET+1;
+    static final int SIBLING_OFFSET = NO_KEYS_OFFSET+2;
+    static final int NODE_HEADER_SIZE = SIBLING_OFFSET+8;
+
+    static final int BALANCE_NUMBER = 32;
+    static final int NODE_SIZE = NODE_HEADER_SIZE + (2*8)*(BALANCE_NUMBER*2+2);
+
+    BPlusTree tree;
+    DataItem dataItem;
+    SubArray raw;
+    long uid;
+
+    static void setRawIsLeaf(SubArray raw, boolean isLeaf) {
+        if(isLeaf) {
+            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte)1;
+        } else {
+            raw.raw[raw.start + IS_LEAF_OFFSET] = (byte)0;
+        }
+    }
+
+    static boolean getRawIfLeaf(SubArray raw) {
+        return raw.raw[raw.start + IS_LEAF_OFFSET] == (byte)1;
+    }
+
+    static void setRawNoKeys(SubArray raw, int noKeys) {
+        System.arraycopy(Parser.short2Byte((short)noKeys), 0, raw.raw, raw.start+NO_KEYS_OFFSET, 2);
+    }
+
+    static int getRawNoKeys(SubArray raw) {
+        return (int)Parser.parseShort(Arrays.copyOfRange(raw.raw, raw.start+NO_KEYS_OFFSET, raw.start+NO_KEYS_OFFSET+2));
+    }
+
+    static void setRawSibling(SubArray raw, long sibling) {
+        System.arraycopy(Parser.long2Byte(sibling), 0, raw.raw, raw.start+SIBLING_OFFSET, 8);
+    }
+
+    static long getRawSibling(SubArray raw) {
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, raw.start+SIBLING_OFFSET, raw.start+SIBLING_OFFSET+8));
+    }
+
+    static void setRawKthSon(SubArray raw, long uid, int kth) {
+        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
+        System.arraycopy(Parser.long2Byte(uid), 0, raw.raw, offset, 8);
+    }
+
+    static long getRawKthSon(SubArray raw, int kth) {
+        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
+    }
+
+    static void setRawKthKey(SubArray raw, long key, int kth) {
+        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
+        System.arraycopy(Parser.long2Byte(key), 0, raw.raw, offset, 8);
+    }
+
+    static long getRawKthKey(SubArray raw, int kth) {
+        int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
+        return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
+    }
+
+    static void copyRawFromKth(SubArray from, SubArray to, int kth) {
+        int offset = from.start+NODE_HEADER_SIZE+kth*(8*2);
+        System.arraycopy(from.raw, offset, to, to.start+NODE_HEADER_SIZE, from.end-from.start-offset);
+    }
+
+    static void shiftRawKth(SubArray raw, int kth) {
+        int begin = raw.start+NODE_HEADER_SIZE+(kth+1)*(8*2);
+        int end = raw.start+NODE_SIZE-1;
+        for(int i = end; i >= begin; i --) {
+            raw.raw[i] = raw.raw[i-(8*2)];
+        }
+    }
+
+    static byte[] newRootRaw(long left, long right, long key)  {
+        SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
+
+        setRawIsLeaf(raw, false);
+        setRawNoKeys(raw, 2);
+        setRawSibling(raw, 0);
+        setRawKthSon(raw, left, 0);
+        setRawKthKey(raw, key, 0);
+        setRawKthSon(raw, right, 1);
+        setRawKthKey(raw, -1, 1);
+
+        return raw.raw;
+    }
+
+    static byte[] newNilRootRaw()  {
+        SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
+
+        setRawIsLeaf(raw, true);
+        setRawNoKeys(raw, 0);
+        setRawSibling(raw, 0);
+
+        return raw.raw;
+    }
+
+    static Node loadNode(BPlusTree bTree, long uid) throws Exception {
+        DataItem di = null;
+        try {
+            di = bTree.dm.read(uid);
+        } catch(Exception e) {
+            throw e;
+        }
+        assert di != null;
+        Node n = new Node();
+        n.tree = bTree;
+        n.dataItem = di;
+        n.raw = di.data();
+        n.uid = uid;
+        return n;
+    }
+
+    public void release() {
+        dataItem.release();
+    }
+
+    public boolean isLeaf() {
+        dataItem.rLock();
+        try {
+            return getRawIfLeaf(raw);
+        } finally {
+            dataItem.rUnLock();
+        }
+    }
+
+    class SearchNextRes {
+        long uid;
+        long siblingUid;
+    }
+
+    public SearchNextRes searchNext(long key) {
+        dataItem.rLock();
+        try {
+
+            SearchNextRes res = new SearchNextRes();
+            int noKeys = getRawNoKeys(raw);
+            for(int i = 0; i < noKeys; i ++) {
+                long ik = getRawKthKey(raw, i);
+                if(key < ik) {
+                    res.uid = getRawKthSon(raw, i);
+                    res.siblingUid = 0;
+                    return res;
+                }
+            }
+            res.uid = 0;
+            res.siblingUid = getRawSibling(raw);
+            return res;
+
+        } finally {
+            dataItem.rUnLock();
+        }
+    }
+
+    class LeafSearchRangeRes {
+        List<Long> uids;
+        long siblingUid;
+    }
+
+    public LeafSearchRangeRes leafSearchRange(long leftKey, long rightKey) {
+        dataItem.rLock();
+        try {
+            int noKeys = getRawNoKeys(raw);
+            int kth = 0;
+            while(kth < noKeys) {
+                long ik = getRawKthKey(raw, kth);
+                if(ik >= leftKey) break;
+                kth ++;
+            }
+            List<Long> uids = new ArrayList<>();
+            while(kth < noKeys) {
+                long ik = getRawKthKey(raw, kth);
+                if(ik <= rightKey) {
+                    uids.add(getRawKthSon(raw, kth));
+                    kth ++;
+                } else {
+                    break;
+                }
+            }
+            long siblingUid = 0;
+            if(kth == noKeys) {
+                siblingUid = getRawSibling(raw);
+            }
+            LeafSearchRangeRes res = new LeafSearchRangeRes();
+            res.uids = uids;
+            res.siblingUid = siblingUid;
+            return res;
+        } finally {
+            dataItem.rUnLock();
+        }
+    }
+
+    class InsertAndSplitRes {
+        long siblingUid, newSon, newKey;
+    }
+
+    public InsertAndSplitRes insertAndSplit(long uid, long key) throws Exception {
+        boolean success = false;
+        Exception err = null;
+        InsertAndSplitRes res = new InsertAndSplitRes();
+
+        dataItem.before();
+        try {
+            success = insert(uid, key);
+            if(!success) {
+                res.siblingUid = getRawSibling(raw);
+                return res;
+            }
+            if(needSplit()) {
+                try {
+                    SplitRes r = split();
+                    res.newKey = r.newKey;
+                    res.newSon = r.newSon;
+                    return res;
+                } catch(Exception e) {
+                    err = e;
+                    throw e;
+                }
+            } else {
+                return res;
+            }
+        } finally {
+            if(err == null && success) {
+                dataItem.after(TransactionManagerImpl.SUPER_XID);
+            } else {
+                dataItem.unBefore();
+            }
+        }
+    }
+
+    private boolean insert(long uid, long key) {
+        int noKeys = getRawNoKeys(raw);
+        int kth = 0;
+        while(kth < noKeys) {
+            long ik = getRawKthKey(raw, kth);
+            if(ik < key) {
+                kth ++;
+            } else {
+                break;
+            }
+        }
+        if(kth == noKeys && getRawSibling(raw) != 0) return false;
+
+        if(getRawIfLeaf(raw)) {
+            shiftRawKth(raw, kth);
+            setRawKthKey(raw, key, kth);
+            setRawKthSon(raw, uid, kth);
+            setRawNoKeys(raw, noKeys+1);
+        } else {
+            long kk = getRawKthKey(raw, kth);
+            setRawKthKey(raw, key, kth);
+            shiftRawKth(raw, kth+1);
+            setRawKthKey(raw, kk, kth+1);
+            setRawKthSon(raw, uid, kth+1);
+            setRawNoKeys(raw, noKeys+1);
+        }
+        return true;
+    }
+
+    private boolean needSplit() {
+        return BALANCE_NUMBER*2 == getRawNoKeys(raw);
+    }
+
+    class SplitRes {
+        long newSon, newKey;
+    }
+
+    private SplitRes split() throws Exception {
+        SubArray nodeRaw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
+        setRawIsLeaf(nodeRaw, getRawIfLeaf(raw));
+        setRawNoKeys(nodeRaw, BALANCE_NUMBER);
+        setRawSibling(nodeRaw, getRawSibling(raw));
+        copyRawFromKth(raw, nodeRaw, BALANCE_NUMBER);
+        long son = 0;
+        try {
+            son = tree.dm.insert(TransactionManagerImpl.SUPER_XID, nodeRaw.raw);
+        } catch(Exception e) {
+            throw e;
+        }
+        setRawNoKeys(raw, BALANCE_NUMBER);
+        setRawSibling(raw, son);
+
+        SplitRes res = new SplitRes();
+        res.newSon = son;
+        res.newKey = getRawKthKey(nodeRaw, 0);
+        return res;
+    }
+
+}
